2ed832a7094c8c0cd33f62fc5cca9345397b54af
FELONYANGO <felixonyango951@gmail.com>
2024-03-10 18:43:48 +0300

    added the taiwind and axios

---
+++ frontend/src/components/home.vue
@@ -0,0 +1,37 @@
+<template>
+
+
+    <h1 class="text-3xl font-bold underline"> {{ msg  }}</h1>
+    
+
+</template>
+    
+    
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
